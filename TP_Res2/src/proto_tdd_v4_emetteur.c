e772689c406714149aa0e18b1739c6fa3df668fa
Varga-CodeAnon <editionpirated@protonmail.com>
2020-04-08 18:17:19 +0200

    Emetteur premier jet

--- TP_Res2/src/proto_tdd_v4_emetteur.c
+++ TP_Res2/src/proto_tdd_v4_emetteur.c
@@ -13,7 +13,7 @@
 #include "couche_transport.h"
 #include "services_reseau.h"
 
-// Côté émetteur, il faut désormais un temporisateur par paquet.
+// Côté émetteur, il faut désormais un temporisateur par paquet. TODO: OK!
 
 // Lors de l'expiration d'un temporisateur on ne renvoi que le
 // paquet correspondant et donc pas toute la fenêtre /!\ TODO:
@@ -21,7 +21,7 @@
 // Quand on reçoit un ACK, il ne vaut que pour ce paquet.
 // Donc il y a deux solutions :
 // - si ce n'est pas le premier paquet en attente d'ack, on met à
-//   vrai un booléen (TODO:) dans un tableau ack_recu (TODO:).
+//   vrai un booléen (TODO:) dans un tableau ack_recu (TODO: OK! ).
 // - si c'est le premier paquet, on fait avancer la fenêtre tant
 //   qu'on le peut. C'est à dire pour ce paquet et pour tous ceux 
 //   précédemment reçus (connus grâce au tableau ack_recu) TODO:
@@ -35,12 +35,13 @@ int main(int argc, char* argv[])
     int taille_msg; /* taille du message */
     int code_retour; /* code de retour de la fonction attendre */
     paquet_t reponse;
-    paquet_t tableau_de_paquet[SEQ_NUM_SIZE]; /* fenêtre de 8 paquets */
+    paquet_t tableau_de_paquet[SEQ_NUM_SIZE]; /* fenêtre de 16 paquets */
     int borne_inf;
     unsigned int curseur;
     int valide;
     init_reseau(EMISSION);
-
+    int tab_ack_recu[SEQ_NUM_SIZE];
+    int ack_attendu = 0;
     printf("[TRP] Initialisation reseau : OK.\n");
     printf("[TRP] Debut execution protocole transport.\n");
 
@@ -65,34 +66,37 @@ int main(int argc, char* argv[])
 
             vers_reseau(&tableau_de_paquet[curseur]);
             
-            if (borne_inf == curseur){
-                depart_temporisateur(1,50); 
-            }
+            depart_temporisateur(curseur,50);  // on démarre un temporisateur par paquet.
+            
             curseur = (curseur + 1)%SEQ_NUM_SIZE;
             de_application(message, &taille_msg);
         }
-        else {  /* On envoi tout sans attendre, puis une fois tout envoyé, on attend */
+        else {  /* Dès que curseur sort de la fenêtre, on envoi tout sans attendre, puis une fois tout envoyé, on attend */
             code_retour = attendre();
-            if (code_retour == PAQUET_RECU){
+            if (code_retour == PAQUET_RECU){  /* Si on reçoit un ACK... */
                 de_reseau(&reponse);
-                if (verifier_controle(reponse) && dans_fenetre(borne_inf,reponse.num_seq,TAILLE_FEN)){
-                    /* décalage de la fenêtre */
-                    borne_inf = (reponse.num_seq + 1) % SEQ_NUM_SIZE;
-                    arreter_temporisateur(1);
-                    if (borne_inf != curseur){
-                        /* Reste des paquets non acquittés, on relance le timer */
-                        depart_temporisateur(1,50);
+                if (verifier_controle(reponse) && dans_fenetre(borne_inf,reponse.num_seq,TAILLE_FEN)){  /* ... et qu'il est conforme */
+                    if (ack_attendu == reponse.num_seq){  /* S'il correspond au paquet attendu */
+                        tab_ack_recu[reponse.num_seq] = 1;
+                        while (tab_ack_recu[ack_attendu]==1){ // tant que les acquittements attendu ont été reçu, on décale la fenêtre
+                            /* décalage de la fenêtre */
+                            arreter_temporisateur(ack_attendu);
+                            borne_inf = ack_attendu;
+                            ack_attendu = (ack_attendu+1) % SEQ_NUM_SIZE;
+                        }  
+                    }
+                    else {  // Sinon on met à vrai le booléen dans tab_ack_recu
+                        arreter_temporisateur(reponse.num_seq);
+                        tab_ack_recu[reponse.num_seq] = 1;
                     }
                 }
             }
             else {
-                /* Cas du timeout : réemission de tout jusqu'au curseur (go-back-n) */
+                /* Cas du timeout : réemission du paquet manquant (selective repeat) */
                 int i = borne_inf;
-                depart_temporisateur(1,50);
-                while (i != curseur){
-                    vers_reseau(&tableau_de_paquet[i]);
-                    i = (i+1) % SEQ_NUM_SIZE;
-                }
+                depart_temporisateur(borne_inf,50);
+                vers_reseau(&tableau_de_paquet[borne_inf]);
+                i = (i+1) % SEQ_NUM_SIZE;
             }
         }
     }
